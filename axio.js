3a209c025f13228b630534004efa48aa3931c1d7
Neha <76679712+Neha-Bhardw@users.noreply.github.com>
2021-01-12 11:12:31 +0530

    Add files via upload

---
+++ axio.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI)
+}
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const ress = await axios.get('https://icanhazdadjoke.com/', config)
+        return ress.data.joke;
+    } catch (e) {
+        return "No Jokes available, Sorry! :("
+    }
+}
